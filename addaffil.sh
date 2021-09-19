4a5526ae1be84bdfbfd0f55782fd3693498f399a
D3STY <nitram88@gmx.net>
2021-09-20 00:31:03 +0200

    clean up

--- addaffil.sh
+++ addaffil.sh
@@ -16,48 +16,47 @@ glftpd_conf="/etc/glftpd.conf"
 # the default pre path.
 base_pre_path="/site/PRE"
 
-
 ### CODE ###
 
 if [ $# -ge 1 ]; then
-   if [ $# -eq 2 ]; then
-	pre_path=$2
-   else
-	pre_path=$base_pre_path
-   fi
-   if [ `expr substr $pre_path 1 5`  != "/site" ]; then
-	if [ `expr substr $pre_path 1 1`  != "/" ]; then
-	   pre_path="/site/$pre_path"
+	if [ $# -eq 2 ]; then
+		pre_path=$2
 	else
-	   pre_path="/site$pre_path"
+		pre_path=$base_pre_path
 	fi
-   fi	
-   echo "Adding $1 ..."
-   if [ `grep "privpath $pre_path" $glftpd_conf | grep -c $1` -gt 0 ]; then
-	echo "The $pre_path/$1 line already exists in $glftpd_conf."
-   else
-	echo "Trying to add $pre_path/$1 to $glftpd_conf ..."
-	/bin/addaffil $glftpd_conf $1 $pre_path
-   fi
-   if [ -d "$pre_path/$1" ]; then
-	echo "The dir $pre_path/$1 already exists, making sure it has permissions set to 777 ..."
-	chmod 777 "$pre_path/$1"
-	echo "Couldn't create $pre_path/$1 dir since it already existed. permissions got updated to 777."
-	echo "Group $1 can start preing now!!!"
-   else
-	mkdir -m777 "$pre_path/$1" >/dev/null 2>&1
-	mkdirres=$?
-	if [ $mkdirres -ne 0 ]; then
-		echo "Error! Couldn't create $pre_path/$1."
-		echo "Removing the $pre_path/$1 dir from $glftpd_conf ..." 
-		lines_num=`cat $glftpd_conf | wc -l`
-		/bin/delaffil $glftpd_conf $1 $pre_path $lines_num
-		echo "Group $1 wasn't set as an affil and it can't pre."
+	if [ $(expr substr $pre_path 1 5) != "/site" ]; then
+		if [ $(expr substr $pre_path 1 1) != "/" ]; then
+			pre_path="/site/$pre_path"
+		else
+			pre_path="/site$pre_path"
+		fi
+	fi
+	echo "Adding $1 ..."
+	if [ $(grep "privpath $pre_path" $glftpd_conf | grep -c $1) -gt 0 ]; then
+		echo "The $pre_path/$1 line already exists in $glftpd_conf."
 	else
-		echo "The $pre_path/$1 dir has been created."
+		echo "Trying to add $pre_path/$1 to $glftpd_conf ..."
+		/bin/addaffil $glftpd_conf $1 $pre_path
+	fi
+	if [ -d "$pre_path/$1" ]; then
+		echo "The dir $pre_path/$1 already exists, making sure it has permissions set to 777 ..."
+		chmod 777 "$pre_path/$1"
+		echo "Couldn't create $pre_path/$1 dir since it already existed. permissions got updated to 777."
 		echo "Group $1 can start preing now!!!"
+	else
+		mkdir -m777 "$pre_path/$1" >/dev/null 2>&1
+		mkdirres=$?
+		if [ $mkdirres -ne 0 ]; then
+			echo "Error! Couldn't create $pre_path/$1."
+			echo "Removing the $pre_path/$1 dir from $glftpd_conf ..."
+			lines_num=$(cat $glftpd_conf | wc -l)
+			/bin/delaffil $glftpd_conf $1 $pre_path $lines_num
+			echo "Group $1 wasn't set as an affil and it can't pre."
+		else
+			echo "The $pre_path/$1 dir has been created."
+			echo "Group $1 can start preing now!!!"
+		fi
 	fi
-   fi  
 else
-   echo "Syntax: SITE ADDAFFIL <group> [pre_dir_path]"
+	echo "Syntax: SITE ADDAFFIL <group> [pre_dir_path]"
 fi
--- getmp3preinfo.sh
+++ getmp3preinfo.sh
@@ -9,12 +9,11 @@
 
 sitename="RG"
 
-
 ### CODE ###
 
-fname=`ls "$1" | grep \[$sitename\]`
+fname=$(ls "$1" | grep \[$sitename\])
 
-first=`echo $fname | awk -F" - COMPLETE - " '{ print $1 }'`
-second=`echo $fname | awk -F" - COMPLETE - " '{ print $2 }'`
-output=`echo $second | awk -F" ) - " '{ print $1 }'`
+first=$(echo $fname | awk -F" - COMPLETE - " '{ print $1 }')
+second=$(echo $fname | awk -F" - COMPLETE - " '{ print $2 }')
+output=$(echo $second | awk -F" ) - " '{ print $1 }')
 echo $output
