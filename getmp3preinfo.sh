6fd98adaa3dbbcf3efcedd07dbae1708ff81fa41
D3STY <nitram88@gmx.net>
2021-09-21 11:47:03 +0200

    mp3genre fix

--- getmp3preinfo.sh
+++ getmp3preinfo.sh
@@ -3,5 +3,5 @@ sitename=""
 fname=$(ls "$1"/*$sitename*)
 
 second=$(echo "$fname" | awk -F" - COMPLETE - " '{ print $2 }')
-output=$(echo "$second" | awk -F" ) - " '{ print $1 }')
+output=$(echo "$second" | awk -F" ) - " '{ print $1 }' | tr -d '[:digit:]')
 echo "$output"
